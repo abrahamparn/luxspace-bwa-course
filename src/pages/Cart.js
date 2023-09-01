f48a3ef80d60a9149ded2b71ada351d427e6d348
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2023-09-01 16:09:53 +0700

    Adding cart page

---
+++ src/pages/Cart.js
@@ -0,0 +1,31 @@
+import react from "react";
+import Header from "parts/Header";
+import Breadcrumb from "components/Breadcrumb";
+
+import ShoppingCart from "parts/Cart/ShoppingCart";
+import ShippingDetails from "parts/Cart/ShippingDetails";
+import Footer from "parts/Sitemap";
+import Sitemap from "parts/Footer";
+export default function HomePage(){
+    return(
+        <>
+            <Header theme = "black"/>
+            <Breadcrumb
+                list={[
+                { url: "/", name: "Home" },
+                { url: "/cart", name: "Shopping Cart" },
+                ]}></Breadcrumb>
+               <section class="md:py-16">
+                    <div class="container mx-auto px-4">
+                        <div class="flex -mx-4 flex-wrap">
+                        <ShoppingCart/>
+                        <ShippingDetails/>
+                        
+                        </div>
+                    </div>
+               </section>
+            <Sitemap/> 
+            <Footer/>
+        </>
+    )
+} 
