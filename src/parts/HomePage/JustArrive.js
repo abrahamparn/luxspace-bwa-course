10582fa31525620133e6e1be8cf1520aaf94536d
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2023-09-12 21:52:26 +0700

    Finish Carousel and Skeleton for Just Arrive

--- src/parts/HomePage/JustArrive.js
+++ src/parts/HomePage/JustArrive.js
@@ -3,6 +3,22 @@ import React, {useEffect, useRef} from 'react'
 import fetch from 'helpers/fetch'
 import { Link } from 'react-router-dom'
 import Carousel from 'components/Carousel'
+
+function Loading(){
+  return Array(6).fill().map((_, index) =>{
+    return <div className='px-4 relative card group' key={index}>
+      <div
+        className="rounded-xl bg-gray-300 overflow-hidden card-shadow relative"
+        style={{ width: 287, height: 386 }}
+      >
+     
+      </div>
+      <div className='w-24 h-3 bg-gray-300 mt-3 rounded-full'></div>
+      <div className='w-36 h-3 bg-gray-300 mt-2 rounded-full'></div>
+
+    </div>
+  })  
+}
 export default function JustArrive() {
 
   const {data, status, error, run, isLoading } = useAsync()
@@ -27,8 +43,9 @@ export default function JustArrive() {
 
         {
           isLoading  
-          ?       <div className="flex -mx-4 flex-row relative">
-          Loading </div>
+          ?       <div className="flex -mx-4 flex-row relative"   style={{ paddingLeft: refContainer.current?.getBoundingClientRect?.().left - 16 || 0 }}
+          >
+          <Loading></Loading> </div>
           : error
           ? JSON.stringify(error) 
           : data.data.length === 0 
@@ -64,7 +81,7 @@ export default function JustArrive() {
                       />
                     </div>
                     <h5 className="text-lg font-semibold mt-4">{item.title}</h5>
-                    <span className="">{item.price}</span>
+                    <span className="">IDR {item.price}</span>
                     <Link
                       to={`/categories/${item.idc}/products/${item.id}`}
                       className="stretched-link"
