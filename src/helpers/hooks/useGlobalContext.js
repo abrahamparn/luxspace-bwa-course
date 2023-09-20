d78e3af76cce2d37cc96597ed6043a19f2387ff2
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2023-09-20 12:56:41 +0700

    update product details page

--- src/helpers/hooks/useGlobalContext.js
+++ src/helpers/hooks/useGlobalContext.js
@@ -1,40 +1,45 @@
-import React, {createContext, useContext, useReducer} from 'react'
+import React, { createContext, useContext, useReducer } from "react";
 
+const Context = createContext();
 
-const Context = createContext()
 const initialState = {
-    cart: {}
+  cart: {},
+};
+
+export function useGlobalContext() {
+  const [state, dispatch] = useContext(Context);
+
+  if (!state || !dispatch) {
+    throw new Error("useGlobalContext must be used within a Provider");
+  }
+
+  return { state, dispatch };
 }
 
-export  function useGlobalContext() {
-    const [state, dispatch] = useContext(Context)
-    if(!state || !dispatch){
-            throw new Error(`useGlobalContext must be inside the provider`)
-    }
-  return (
-    {state, dispatch}
-    )
-}
-function Reducer(state, action){
-    switch (action.type) {
-        case "ADD_TO_CHART":
-            
-            return {...state,
-                cart: state.cart ? {
-                    ...state.cart, [action.item.id] : action.item
-                } : {
-                    [action.item.id] : action.item
-                }
+function Reducer(state, action) {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      console.log(action);
+      return {
+        ...state,
+        cart: state.cart
+          ? {
+              ...state.cart,
+              [action.item.id]: action.item,
             }
-    
-        default:{
-            throw new Error(`Unhandled  action type ${action.type}`)
-        }
+          : { [action.item.id]: action.item },
+      };
+
+   
+
+    default: {
+      throw new Error(`Unhandled action type ${action.type}`);
     }
+  }
 }
 
-
-export default function  Provider(props){
-     const [state, dispatch] = useReducer(Reducer, initialState)
-     return <Context.Provider value={[state, dispatch]}{...props}/>
+export default function Provider(props) {
+  const [state, dispatch] = useReducer(Reducer, initialState);
+  // console.log(state);
+  return <Context.Provider value={[state, dispatch]} {...props} />;
 }
--- src/parts/Details/ProductDetails.js
+++ src/parts/Details/ProductDetails.js
@@ -47,11 +47,15 @@ export default function ProductDetails(data) {
         <p className="text-xl">IDR {data.data.price}</p>
 
         <button
-          className="transition-all duration-200 bg-pink-400 text-black focus:bg-black focus:text-pink-400 rounded-full px-8 py-3 mt-4 inline-flex"
-            onClick={
-              () => dispatch({type: "ADD_TO_CHART", item:data.data})
+            className="transition-all duration-200 bg-pink-400 text-black focus:bg-black focus:text-pink-400 rounded-full px-8 py-3 mt-4 inline-flex"
+            onClick={() =>
+              dispatch({
+                type: "ADD_TO_CART",
+                item: data.data,
+              })
             }
-          ><svg
+          >
+          <svg
             className="fill-current mr-3"
             width="26"
             height="24"
